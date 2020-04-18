9d4012e86ada447570cb469282404070a33dfb61
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-04-18 17:20:27 +0700

    Added some comments

--- src/main/java/com/avatarduel/model/player/Stats.java
+++ src/main/java/com/avatarduel/model/player/Stats.java
@@ -1,26 +1,49 @@
 package com.avatarduel.model.player;
 
+/**
+ * Stats is the class that defines a stats attribute for the player.
+ * @author feliciagojali
+ */
 public class Stats {
     private int current;
     private int max;
 
+    /**
+     * The default stats constructor
+     */
     public Stats(){
         this.current = 99;
         this.max = 99;
     }
+
+    /**
+     * Getter for current stats
+     * @return integer of current stats value
+     */
     public int getCurrent(){
         return this.current;
     }
 
+    /**
+     * Getter for the max value of stats
+     * @return integer of max stats value
+     */
     public int getMax(){
         return this.max;
     }
 
-    public void setCurrent(int x){
-        this.current = x;
+    /**
+     * Setter for current stats value
+     * @param value
+     */
+    public void setCurrent(int value){
+        this.current = value;
     }
-
-    public void setMax(int x){
-        this.max = x;
+    /**
+     * Setter for max value
+     * @param value
+     */
+    public void setMax(int value){
+        this.max = value;
     }
 }
